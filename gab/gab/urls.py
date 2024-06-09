7f957872e0cac88624337e3926ff453befeee023
Purity Jangaya <purityjangaya@gmail.com>
2024-06-09 17:04:26 +0300

    added staticfiles

--- gab/gab/urls.py
+++ gab/gab/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('mentorship/', include("mentorship.urls")),
@@ -25,3 +26,4 @@ urlpatterns = [
     path("__reload__/", include("django_browser_reload.urls")),
     
 ]
+urlpatterns += staticfiles_urlpatterns()
